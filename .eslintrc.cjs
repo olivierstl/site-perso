4c659288ebe365ca14fb60e8426d187debf21d36
Olivier <olivier.stl@proton.me>
2023-04-07 17:33:19 +0200

    chore: install astro eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:astro/recommended',
     '@vue/eslint-config-typescript',
     // Gère l'ordre de tous les plugins installés
     'alsacreations'
@@ -15,6 +16,25 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+    // ...
+  ],
   rules: {
     "@typescript-eslint/prefer-function-type": "off",
     "vue/custom-event-name-casing": ["error", "kebab-case"]
